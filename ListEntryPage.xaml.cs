75e9a3780e648f1123eca1645fc8a27c4845372e
Taiyo200 <petrusvlad01@gmail.com>
2023-12-04 17:37:11 +0200

    Lab8

---
+++ ListEntryPage.xaml.cs
@@ -0,0 +1,39 @@
+using SQLite;
+using Petrus_Vlad_Darius_Lab7.Model;
+
+namespace Petrus_Vlad_Darius_Lab7;
+
+public partial class ListEntryPage : ContentPage
+{
+	public ListEntryPage()
+	{
+		InitializeComponent();
+	}
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        listView.ItemsSource = await App.Database.GetShopListsAsync();
+    }
+
+    async void OnShopListAddedClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ListPage
+        {
+            BindingContext = new ShopList()
+        });
+    }
+
+    async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem != null)
+        {
+            await Navigation.PushAsync(new ListPage
+            {
+                BindingContext = e.SelectedItem as ShopList
+            });
+        }
+
+    }
+
+}
